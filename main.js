1d918b0b772634f965d6ef50df75f79d11cf8f8a
DebojyotiDhar <97465237+DebojyotiDhar@users.noreply.github.com>
2022-07-06 20:28:53 +0530

    initial comment

---
+++ main.js
@@ -0,0 +1,15 @@
+//const Mode_icon = document.getElementById('Mode');
+
+const Switch_Mode = document.getElementById('switch');
+const Inner_text = document.getElementById('inner-text');
+Mode.onclick = function () {
+    document.body.classList.toggle('dark-mode');
+    Switch_Mode.classList.toggle('switch-active');
+    Inner_text.classList.toggle('Inner-text-active');
+    if (document.body.classList.contains('dark-mode')) {
+        Inner_text.innerHTML = 'Light';
+    }
+    else {
+        Inner_text.innerHTML = 'Dark';
+    }
+}
